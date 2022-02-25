3e4b4ccb8758fd02e9b0ffa94a139ef49e176a36
3Sm0k1nG2 <3sm0k1ng2@gmail.com>
2022-02-25 18:02:39 +0200

    Fixed: script.js is working as intented. code could be refactored

--- script.js
+++ script.js
@@ -4,51 +4,114 @@ let selectElement = document.getElementById('ctl00_ContentPlaceHolder2_dd_zl');
 let isLoaded = selectElement.options.selectedIndex === 1;
 injector();
 
-// let newBtnElement = document.createElement('button');
-// const divElement = document.getElementById('ctl00_ContentPlaceHolder2_Panel7').children[0];
-// divElement.append(newBtnElement);
-// newBtnElement.addEventListener('click', injector());
+
+function createAndAppendNewButtons() {
+    appendButtonToDiv(createChooseLabSelect());
+    appendButtonToDiv(createChooseSemSelect());
+}
+
+function createChooseLabSelect() {
+    const newSelectElement = document.createElement('select');
+    newSelectElement.id = 'lab-btn';
+
+    for (let i = 1; i <= 3; i++) {
+        let newOptionElement = document.createElement('option');
+        newSelectElement.appendChild(newOptionElement);
+        newOptionElement.value = `${i} лаб`;
+        newOptionElement.textContent = `${i} лаб`;
+    }
+
+    newSelectElement.addEventListener('change', (e) => {
+        let indexSem = e.target.options.selectedIndex + 1;
+
+        getFilterSubjectsByGroup(indexSem);
+        filterSubjects(indexSem);
+
+        let newSelectElement = document.getElementById('sem-btn');
+        newSelectElement.hidden = indexSem === 2 ? false : true;
+    })
+
+    return newSelectElement;
+}
+
+function appendButtonToDiv(buttonElement){
+    const divElement = document.getElementById('ctl00_ContentPlaceHolder2_Panel7').children[0];
+
+    let lastChildElement = divElement.children[divElement.children.length - 1];
+    divElement.appendChild(buttonElement);
+    divElement.appendChild(lastChildElement);
+}
+
+function createChooseSemSelect() {
+
+    const newSelectElement = document.createElement('select');
+    newSelectElement.id = 'sem-btn';
+    newSelectElement.hidden = true;
+    
+    for (let i = 1; i <= 2; i++) {
+        let newOptionElement = document.createElement('option');
+        newSelectElement.appendChild(newOptionElement);
+        newOptionElement.value = `${i} сем`;
+        newOptionElement.textContent = `${i} сем`;
+    }
+
+    newSelectElement.addEventListener('change', (e) => {
+        console.log(document.getElementById('lab-btn'));
+        console.log(e.target.options.selectedIndex + 1);
+        filterSubjects(document.getElementById('lab-btn').options.selectedIndex+1, e.target.options.selectedIndex + 1);
+    })
+
+    return newSelectElement;
+}
+
 
 function injector() {
     //alert("It works!");
 
     // TODO:
     // 1. Auto change season - DONE
-    // 2.
-    // Add a button to choose which 'лаб' u want to be shown.
-    // Get subject selected from the button option.
-    // 3.
-    // Add another button to choose which 'сем' u want to be shown.
-
-    // createButton;
+    // 2. Add select 'лаб' - DONE
+    // 3. Add select 'сем' - DONE
 
     let semesterSeason = getSemesterSeason();
     setSemesterSeason(semesterSeason);
 
-    let filteredSubjects = getFilterSubjectsByGroup();
-    filteredSubjects = Array.from(filteredSubjects).map((td) => td.parentElement);
-    changeBGColor(filteredSubjects, 'gold');
+    filterSubjects();
 
+    createAndAppendNewButtons();
 };
 
-function changeBGColor(elements, color) {
+function filterSubjects(groupNumberLab = getGroupNumberLab(), groupNumberSem = getGroupNumberSem()){
+    let filteredSubjects = getFilterSubjectsByGroup(groupNumberLab, groupNumberSem);
+    filteredSubjects = Array.from(filteredSubjects).map((td) => td.parentElement);
+
+    clearBGColor();
+    changeBGColor(filteredSubjects, 'gold');
+}
+
+function clearBGColor(){
+    const DEFAULT_COLOR = 'white';
+    const selector = 'tr>td:nth-of-type(4)';
+    let elements = document.querySelectorAll(selector);
+    elements = Array.from(elements).map( (el) => el.parentElement);
 
     elements.forEach((el) => {
-        el.style.backgroundColor = color;
-        console.dir(el.style);
+        el.style.backgroundColor = DEFAULT_COLOR;
     });
 }
 
-function getFilterSubjectsByGroup(groupNumberLab = getGroupNumberLab()) {
+function changeBGColor(elements, color) {
+    elements.forEach((el) => {
+        el.style.backgroundColor = color;
+    });
+}
 
-    if(!(groupNumberLab >= 1 && groupNumberLab <= 3))
+function getFilterSubjectsByGroup(groupNumberLab = getGroupNumberLab(), groupNumberSem = getGroupNumberSem()) {
+    if (!(groupNumberLab >= 1 && groupNumberLab <= 3))
         throw new Error('Wrong parameters received.\n groupNumberLab should be (>= 1 && <= 3), groupNumberSem should be (1 || 2)');
 
-    let groupNumberSem;
-
-    switch(groupNumberLab){
+    switch (groupNumberLab) {
         case 1: groupNumberSem = 1; break;
-        case 2: getGroupNumberSem(); break;
         case 3: groupNumberSem = 2; break;
     }
 
@@ -58,7 +121,6 @@ function getFilterSubjectsByGroup(groupNumberLab = getGroupNumberLab()) {
     return Array.from(tdElements).filter((td) => td.innerHTML === '&nbsp;'
         || td.innerText === groupNumberLab + ' лаб'
         || td.innerText === groupNumberSem + ' сем');
-
 }
 
 function getFacultyNumber() {
@@ -69,11 +131,11 @@ function getGroupNumberLab() {
     return Number(document.getElementById('ctl00_fv_studinfo_grupaLabel').textContent);
 }
 
-function getGroupNumberSem(){
+function getGroupNumberSem(groupNumberSem) {
     const FACULTY_NUMBER_THRESHOLD = 2109011056;
     const facultyNumber = getFacultyNumber();
 
-    return facultyNumber <= FACULTY_NUMBER_THRESHOLD ? 1 : 2
+    return groupNumberSem || (facultyNumber <= FACULTY_NUMBER_THRESHOLD ? 1 : 2);
 }
 
 function getSemesterSeason() {
@@ -108,13 +170,8 @@ function getSemesterSeason() {
         }
     }
 
-    console.log(day, month);
-
     for (let semesterName in semesters) {
         semester = semesters[semesterName];
-        console.log(semesterName);
-        console.log(`${semester.start.month} <= ${month} <= ${semester.end.month}`);
-        console.log(`${semester.start.day} <= ${day} <= ${semester.end.day}`);
 
         if ((semester.start.month <= month && month <= semester.end.month) && (semester.start.day <= day && day <= semester.end.day))
             return semesterName;
@@ -124,17 +181,17 @@ function getSemesterSeason() {
 }
 
 function setSemesterSeason(season) {
-    
+
     const WINTER = 'winter';
 
     const selectElement = document.getElementById('ctl00_ContentPlaceHolder2_dd_zl');
-    const onchangeEvent = new Event('change', { });
+    const onchangeEvent = new Event('change', {});
 
     selectElement.options.selectedIndex = season === WINTER ? 0 : 1;
 
-    if(isLoaded)
+    if (isLoaded)
         return;
-    
+
     isLoaded = true;
     selectElement.dispatchEvent(onchangeEvent);
 }
